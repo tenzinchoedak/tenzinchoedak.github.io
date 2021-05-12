c56dfd45ab85f40e8912159b36c2c6d0400dbf62
Tenzin Choedak <tenzin.choedak@powercoders.com>
2021-05-12 21:59:52 +0200

    post my first blog.

--- myScript.js
+++ myScript.js
@@ -7,5 +7,30 @@ hamburger.addEventListener("click", () => {
   navName.classList.toggle("open");
 });
 
+/**this is for the readmore function*/
 
+function readMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
 
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+/**this is for when click change size the image of blogs*/
+let changeSz = document.querySelector(".blogpic");
+function changeSize () {
+  if(changeSz.style.width === "40%"){
+  changeSz.style.width="75%";
+} else {
+  changeSz.style.width="40%";
+}
+}
